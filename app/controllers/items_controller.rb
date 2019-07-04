3a788e3ef5b1caf9259d9befa71fc270d83524d1
Tomonori HIRATA <hirata@sqlab.jp>
2019-07-04 21:54:02 +0900

    update: createアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,41 @@
 class ItemsController < ApplicationController
     def index
-        @items = Item.all
-        respond_to do |format|
-            format.html
-            format.json { render :json => @items }
-        end
+      @items = Item.all
+      respond_to do |format|
+          format.html
+          format.json { render :json => @items }
+      end
     end
 
     def show
-        @item = Item.find(params[:id])
-        respond_to do |format|
-            format.html
-            format.json { render :json => @item }
-        end
+      @item = Item.find(params[:id])
+      respond_to do |format|
+          format.html
+          format.json { render :json => @item }
+      end
     end
+
+    def create
+			@item = Item.new(item_params)
+			if @item.save
+				respond_to do |format|
+          format.html { redirect_to item_path(@item) }
+          format.json { render :json => @item }
+      	end
+			else
+				respond_to do |format|
+          format.html { redirect_to root_path }
+          format.json { render :json => @item.errors }
+      	end
+			end
+
+    end
+
+    private
+
+			def item_params
+				params.require(:item).permit(:title, :description, :price)
+			end
+
 
 end
