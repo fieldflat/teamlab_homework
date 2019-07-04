ac451205812603e835b2e481e32b94542a65eeb9
Tomonori HIRATA <hirata@sqlab.jp>
2019-07-04 22:43:31 +0900

    update: Deleteアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,41 +1,51 @@
 class ItemsController < ApplicationController
-    def index
-      @items = Item.all
+  protect_from_forgery #CSFR対策
+
+  def index
+    @items = Item.all
+    respond_to do |format|
+      format.html
+      format.json { render :json => @items }
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render :json => @item }
+    end
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
       respond_to do |format|
-          format.html
-          format.json { render :json => @items }
+        format.html { redirect_to item_path(@item) }
+        format.json { render :json => @item }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.json { render :json => @item.errors }
       end
     end
-
-    def show
-      @item = Item.find(params[:id])
-      respond_to do |format|
-          format.html
-          format.json { render :json => @item }
-      end
+  end
+  
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    respond_to do |format|
+      format.html { redirect_to items_path }
+      format.json { render :json => { status: "SUCCESS", method: "deleted" } }
     end
+  end
 
-    def create
-			@item = Item.new(item_params)
-			if @item.save
-				respond_to do |format|
-          format.html { redirect_to item_path(@item) }
-          format.json { render :json => @item }
-      	end
-			else
-				respond_to do |format|
-          format.html { redirect_to root_path }
-          format.json { render :json => @item.errors }
-      	end
-			end
+  private
 
+    def item_params
+      params.require(:item).permit(:title, :description, :price)
     end
-
-    private
-
-			def item_params
-				params.require(:item).permit(:title, :description, :price)
-			end
 
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-    validates :title, presence: true, length: {maximum: 100}
-    validates :description, length: {maximum: 500}
-    validates :price, numericality: {greater_than_or_equal_to: 0}
+  validates :title, presence: true, length: {maximum: 100}
+  validates :description, length: {maximum: 500}
+  validates :price, numericality: {greater_than_or_equal_to: 0}
 end
