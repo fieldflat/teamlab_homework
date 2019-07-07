45d28435eb68445b9a4856da20477171caa7d447
Tomonori HIRATA <hirata@sqlab.jp>
2019-07-07 23:26:46 +0900

    update: 商品登録と検索ボタンの切り替えを実装

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,9 @@
+$('#register-btn').click(function(){
+  console.log("sfsf");
+  $('.register').toggleClass("display_none");
+})
+
+$('#search-btn').click(function () {
+  console.log("sdsd");
+  $('.search').toggleClass("display_none");
+})
