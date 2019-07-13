3c124143484794e05af2dae6f477069f33a522a2
Tomonori HIRATA <hirata@sqlab.jp>
2019-07-13 14:17:41 +0900

    update: 業者登録ページを作成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,27 @@
 class ShopsController < ApplicationController
-  def new
+  def show
+    @shop = Shop.find(params[:id])
   end
+
+  def new
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      # 保存の成功をここで扱う。
+      flash[:success] = "Created!"
+      redirect_to @shop
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def shop_params
+      params.require(:shop).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'shops/new'
+  # get 'shops/new'
+  get  '/signup',  to: 'shops#new'
+  post '/signup',  to: 'shops#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#home" # トップページ
 
   resources :items
+  resources :shops
   post "/items/:id", to: "items#update" # 外部からupdate出来るようにpostを追加(PUTは弾かれるため)
 end
